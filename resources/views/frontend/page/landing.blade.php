3730c5db2035ed467bacc82bd597108ca9472c71
Arvin Olivas <arvin.olivas15@gmail.com>
2021-02-23 21:21:33 +0800

    Added Files

---
+++ resources/views/frontend/page/landing.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Printhings Digital</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="page">
+        <div class="main">
+            <div class="header">
+                <div class="container">
+                    <div class="menu-container">
+                        <div class="logo">
+                            <img src="img/logo.png" alt="Printhings Digital">
+                        </div>
+                        <div class="menu">
+                            <button class="btn-order">ORDER NOW</button>
+                        </div>
+                    </div>
+                    <div class="slide-container">
+                        <div class="content">
+                            <div class="text-content">
+                                <h2>All the print you need in one stop</h2>
+                                <h1>Copying and Printing Services</h1>
+                                <button class="btn-services">Our Services</button>
+                            </div>
+                            <div class="img-content">
+                                <img src="img/device.png" alt="printing">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="body">
+                <div class="about">
+                    <div class="container">
+                        <div class="about-img">
+                            <img src="img/about-pic.jpg" alt="Printhings Digital" width="100%">
+                        </div>
+                        <div class="about-text">
+                            <div class="title">Think it. Print it.</div>
+                            <div class="details">
+                                All your printing needs in one place. We offer Offset, Digital and Large Format Printing. Flyers Brochures Business Cards Stickers Magazines Posters Packaging Signages and More. Digital Printing Service.
+                                <br><br>
+                                Since opening our doors, we’ve been committed to providing service of the highest quality, paying particular attention to working efficiently while keeping the lines of communication with our clients clear and concise.
+                                <br><br>
+                                <button class="btn">Read More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="services">
+                    <div class="container">
+                        <h1>What We Do</h1>
+                        <h3>
+                            Printhings Digital offers graphic designing and various printing services.
+                            <br>
+                            All your printing needs, all at one place!
+                        </h3>
+
+                        <div class="row">
+                            <div class="item">
+                                <div class="img-item" style="background:url(img/tarpaulin.jpg);"></div>
+                                <div class="item-details">
+                                    <div class="item-title">Tarpaulin Printing</div>
+                                    <div class="description">
+                                        Our cheap but high quality tarpaulin print for as low as Php 12.00 per sq.ft. We use ecosolvent ink for our tarpaulin print which is very glossy, durable and high quality!
+                                    </div>
+                                    <button class="btn">Click Me</button>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <div class="img-item" style="background:url(img/sticker.jpg);"></div>
+                                <div class="item-details">
+                                    <div class="item-title">Sticker Printing</div>
+                                    <div class="description">
+                                        Cheap, high quality and waterproof sticker for as low as Php 1.00 per sq. inch.
+                                        <br><br>
+                                        Discounts on bulk orders.
+                                    </div>
+                                    <button class="btn">Click Me</button>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <div class="img-item" style="background:url(img/sintra.jpg);"></div>
+                                <div class="item-details">
+                                    <div class="item-title">Sintra Printing</div>
+                                    <div class="description">
+                                        Cheap and high quality sintra board print for as low as Php 1.50 per sq. inch.
+                                        <br><br>
+                                        Discounts on bulk orders.
+                                    </div>
+                                    <button class="btn">Click Me</button>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <div class="img-item" style="background:url(img/shirt.jpg);"></div>
+                                <div class="item-details">
+                                    <div class="item-title">Shirt Printing</div>
+                                    <div class="description">
+                                        Printhings Digital offers a very high quality shirt and print for a very cheap cost. We make sure that our customers would wear it proudly.
+                                        For as low as Php 150.00 per shirt.
+                                    </div>
+                                    <button class="btn">Click Me</button>
+                                </div>
+                            </div>
+
+                            <!-- <div class="item">
+                                <img src="img/mug.jpg" alt="Mug Printing" width="100%">
+                                <div class="item-title">Mug Printing</div>
+                                <div class="description">
+                                    We use high quality sublimation mugs for our Mug Printing, which our customers are very satisfied and happy to use. For as low as Php 60.00 per mug with customized print! Discount on bulk orders.
+                                </div>
+                                <button>Click Me</button>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
